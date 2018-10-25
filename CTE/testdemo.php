ec2460f96beea48441dd15dbe41affbe1ebabb4a
Marie Grace Imanariyo <36137774+mariegrace02@users.noreply.github.com>
2018-10-24 21:49:26 -0400

    Add files via upload

---
+++ CTE/testdemo.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+<form>
+<fieldset>
+<p> This is CTE </p>
+</fieldset>
+</form>
+
+<?php
+echo "Hello World";
+?>
+
+</body>
+</html>
+
