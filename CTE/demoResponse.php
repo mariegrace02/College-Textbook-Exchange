d6dee8bb8bdab7ca4e5daa5a1794bb854666c294
maxkirin <42882285+maxkirin@users.noreply.github.com>
2018-10-30 10:34:31 -0400

    format fixes

--- CTE/demoResponse.php
+++ CTE/demoResponse.php
@@ -9,58 +9,58 @@
 
 <?php 
 
-//EDIT THESE NUMBERS
-//establishing a connection to our server
-$dbhost = '192.168.110.46';
-$dbuser = 'team';
-$dbpass = 'HolaJesus123?';
-$conn = mysql_connect($dbhost, $dbuser, $dbpass);
+	//EDIT THESE NUMBERS
+	//establishing a connection to our server
+	$dbhost = '192.168.110.46';
+	$dbuser = 'team';
+	$dbpass = 'HolaJesus123?';
+	$conn = mysql_connect($dbhost, $dbuser, $dbpass);
 
-//making sure we can connect to our server
-if(! $conn ) {
-   die('Could not connect: ' . mysql_error());
-}
+	//making sure we can connect to our server
+	if(! $conn ) {
+		die('Could not connect: ' . mysql_error());
+	}
 
-/*
+	/*
 		This is a a section to POST inputs from the keyboards
 		of respective variables.
 		
 	*/
-$firstname= $_POST["firstname"];
-$lastname= $_POST["lastname"];
-$username= $_POST["username"];
-$email= $_POST["email"];
-//$phone = $_POST["phone"];
-$password = $_POST["password"];
-$passwordagain = $_POST["passwordagain"];
+	$firstname= $_POST["firstname"];
+	$lastname= $_POST["lastname"];
+	$username= $_POST["username"];
+	$email= $_POST["email"];
+	//$phone = $_POST["phone"];
+	$password = $_POST["password"];
+	$passwordagain = $_POST["passwordagain"];
 
-//NEED TO GENERATE ID BASED OFF OF LAST ID IN THE STUDENT TABLE
+	//NEED TO GENERATE ID BASED OFF OF LAST ID IN THE STUDENT TABLE
 
-//CHECK TO SEE IF PASSWORDS ARE THE SAME
-//ELSE IF CHECK TO SEE IF THE EMAIL WAS ALREADY IN USE
-//ELSE IF CHECK TO SEE IF THE USERNAME IS ALREADY IN USE
-//ELSE CAN ADD TO THE DATABASE
+	//CHECK TO SEE IF PASSWORDS ARE THE SAME
+	//ELSE IF CHECK TO SEE IF THE EMAIL WAS ALREADY IN USE
+	//ELSE IF CHECK TO SEE IF THE USERNAME IS ALREADY IN USE
+	//ELSE CAN ADD TO THE DATABASE
 
-$insert = "INSERT INTO Student"."(firstName, lastName, email, phoneNumber, IDNumber, password)".
-"VALUES"."('$firstname', '$lastname', '$email', '$phone', '$RANDOMLYGENERATEID', '$password')";
+	$insert = "INSERT INTO Student"."(firstName, lastName, email, phoneNumber, IDNumber, password)".
+		"VALUES"."('$firstname', '$lastname', '$email', '$phone', '$RANDOMLYGENERATEID', '$password')";
 
-//not sure what this is for
- mysql_select_db('Student');
+	//not sure what this is for
+	mysql_select_db('Student');
 
-//querying the database
-$sqlinsert = mysql_query($insert, $conn);
+	//querying the database
+	$sqlinsert = mysql_query($insert, $conn);
+	
+	//checking to see if the insert worked
+	if(! $sqlinsert ) {
+		die('Could not enter data: ' . mysql_error());
+	}
 
-//checking to see if the insert worked
-if(! $sqlinsert ) {
-   die('Could not enter data: ' . mysql_error());
-}
+	//$data = $username."-".$password;
+	//$sentences = file ("./names.txt");
 
-//$data = $username."-".$password;
-//$sentences = file ("./names.txt");
+	//$fileName = file_put_contents("./names.txt", rtrim($data), FILE_APPEND);
 
-//$fileName = file_put_contents("./names.txt", rtrim($data), FILE_APPEND);
-
-mysql_close($conn);
+	mysql_close($conn);
 ?>
 
 
